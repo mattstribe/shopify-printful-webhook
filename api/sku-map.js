fb02a8fc1f74a4024438e41bbd65855e93d3c570
mattstribe <mstreibich899@gmail.com>
2025-08-18 14:42:57 -0400

    Update sku-map.js

--- api/sku-map.js
+++ api/sku-map.js
@@ -1,6 +1,6 @@
 // Edit this in GitHub whenever you add products.
 // LEFT = Shopify SKU, RIGHT = Printful variant_id (from your Printful manual store)
 export const skuToVariant = {
-  2025-00001: 24352,
+  "2025-00001": 24352,
   // "MY-HOODIE-BLACK-M": 999999999,
 };
