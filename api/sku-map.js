8490c2526bb3325a9147b33c5f615646503df109
mattstribe <mstreibich899@gmail.com>
2025-08-18 17:31:11 -0400

    Back to ESM

--- api/sku-map.js
+++ api/sku-map.js
@@ -1,7 +1,4 @@
 // LEFT = Shopify SKU, RIGHT = Printful variant_id (from your Printful manual store)
-const skuToVariant = {
+export const skuToVariant = {
   "2025-00001": 24352,
 };
-
-module.exports = { skuToVariant };
-
