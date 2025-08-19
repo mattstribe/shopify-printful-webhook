d4b459e8ed416f375691ca35e4d2bbf8332ab6f4
mattstribe <mstreibich899@gmail.com>
2025-08-19 09:37:59 -0400

    Delete api/sku-map.js

--- api/sku-map.js
+++
@@ -1,9 +0,0 @@
-// LEFT = Shopify SKU, RIGHT = Printful variant_id (from your Printful manual store)
-export const skuToVariant = {
-  "BC3001_WHT_S": 24352,
-  "BC3001_WHT_M": 24353,
-  "BC3001_WHT_L": 24354,
-  "BC3001_WHT_XL": 24355,
-  "BC3001_WHT_2XL": 24356,
-  "BC3001_WHT_3XL": 24357,
-};
