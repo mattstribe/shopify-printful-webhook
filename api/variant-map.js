6cb7976ffa541e3a3b4c369dc821177d06725f0c
mattstribe <mstreibich899@gmail.com>
2025-08-19 09:38:54 -0400

    Create variant-map.js

---
+++ api/variant-map.js
@@ -0,0 +1,11 @@
+// api/variant-map.js
+// LEFT = "PRODUCTCODE_COLOR_SIZE", RIGHT = Printful catalog variant_id
+
+export const productColorSizeToVariant = {
+  "BC3001_WHT_S":   24352,
+  "BC3001_WHT_M":   24353,
+  "BC3001_WHT_L":   24354,
+  "BC3001_WHT_XL":  24355,
+  "BC3001_WHT_2XL": 24356,
+  "BC3001_WHT_3XL": 24357,
+};
