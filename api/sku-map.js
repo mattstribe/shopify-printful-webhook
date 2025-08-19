16bf2ce6e74282d665904bbc7d4bb589819b1ace
mattstribe <mstreibich899@gmail.com>
2025-08-18 23:08:33 -0400

    Update sku-map.js

--- api/sku-map.js
+++ api/sku-map.js
@@ -1,4 +1,9 @@
 // LEFT = Shopify SKU, RIGHT = Printful variant_id (from your Printful manual store)
 export const skuToVariant = {
-  "2025-00001": 24352,
+  "BC3001_WHT_S": 24352,
+  "BC3001_WHT_M": 24353,
+  "BC3001_WHT_L": 24354,
+  "BC3001_WHT_XL": 24355,
+  "BC3001_WHT_2XL": 24356,
+  "BC3001_WHT_3XL": 24357,
 };
