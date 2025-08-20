b7335f5f2750468761f7c881dc725e94042f525f
mattstribe <mstreibich899@gmail.com>
2025-08-20 14:47:56 -0400

    Update variant-map.js

--- api/variant-map.js
+++ api/variant-map.js
@@ -2,10 +2,15 @@
 // LEFT = "PRODUCTCODE_COLOR_SIZE", RIGHT = Printful catalog variant_id
 
 export const productColorSizeToVariant = {
+  // Bella and Canvas 3001 White T-Shirt
+  "BC3001_WHT_XS":  24351,
   "BC3001_WHT_S":   24352,
   "BC3001_WHT_M":   24353,
   "BC3001_WHT_L":   24354,
   "BC3001_WHT_XL":  24355,
   "BC3001_WHT_2XL": 24356,
   "BC3001_WHT_3XL": 24357,
+
+  // Gildan 18500 Black Sweatshirt
+  "G18500_BLK_S": 
 };
