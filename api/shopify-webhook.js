944bdd55e705f2c69a64eb58cdd88615e4176f1a
mattstribe <mstreibich899@gmail.com>
2025-08-21 00:39:31 -0400

    Update shopify-webhook.js

--- api/shopify-webhook.js
+++ api/shopify-webhook.js
@@ -211,7 +211,7 @@ export default async function handler(req, res) {
 
 
   // ----- Build Printful order payload
-  const shouldConfirm = (process.env.PRINTFUL_CONFIRM || "true") === "true";
+  // const shouldConfirm = (process.env.PRINTFUL_CONFIRM || "true") === "true";
   
   const printfulOrder = {
     recipient,
@@ -219,7 +219,7 @@ export default async function handler(req, res) {
     external_id: `shopify-${order.id}`, // useful to cross-reference
     shipping: "STANDARD",
     store_id: Number(process.env.PRINTFUL_STORE_ID),
-    confirm: shouldConfirm,
+    confirm: false,
   };
 
   // ----- Send to Printful
